20f66ef13c39028be645c02a997f4ca39d1e9556
Anas kumar <anaskumar0309@gmail.com>
2026-06-19 22:39:13 +0530

    58 => length-of-last-word

---
+++ LC_58.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        s_list = s.split(" ")
+        return len(s_list[-1])
+        
+        
