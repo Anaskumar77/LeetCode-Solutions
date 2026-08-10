c121c6ca8a62f1d367b5a8041bc61a5e7ab2ae6e
Anas kumar <anaskumar0309@gmail.com>
2026-08-10 21:10:58 +0530

    generate-parentheses

--- LC_22.py
+++ LC_22.py
@@ -1,8 +1,22 @@
-n = 3
-collect = []
-level = 0
-string = []
-n =  n * 2
-def recursive(node):
-    while level < n:
-        string
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        output = []
+
+        def BackTracking(open_count,closing_count):
+            if len(output) == 2*n:
+                res.append(''.join(output))
+                return 
+
+            if open_count < n :
+                output.append('(')
+                BackTracking(open_count + 1 , closing_count)
+                output.pop()
+            
+            if open_count > closing_count:
+                output.append(')')
+                BackTracking(open_count, closing_count + 1)
+                output.pop()
+                
+        BackTracking(0,0)  
+        return res
